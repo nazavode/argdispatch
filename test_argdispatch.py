22664fd16130e173320fcb0e782145b8df67f34a
Federico Ficarelli <federico.ficarelli@gmail.com>
2016-01-02 19:02:06 +0100

    Augmenting tests.

--- test_argdispatch.py
+++ test_argdispatch.py
@@ -14,8 +14,73 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from argdispatch import argdispatch
 import abc
+import inspect
+
+import pytest
+
+from argdispatch import argdispatch
+
+###############################################################################
+# Test functions
+
+DISPATCHED = 'DISPATCHED'
+
+
+def fun_1(a=0, b=1, c=2, d=3, e=4, f=5, g=6):
+    return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f, 'g': g}
+
+
+def fun_2(a=0, b=1, c=2, d=3, e=4, f=5, g=6):
+    return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f, 'g': g}
+
+
+def fun_3(a=0, b=1, c=2, d=3, e=4, f=5, g=6):
+    return {'a': a, 'b': b, 'c': c, 'd': d, 'e': e, 'f': f, 'g': g}
+
+###############################################################################
+# Fixtures
+
+functions = (
+    fun_1,
+    fun_2,
+    fun_3,
+)
+
+
+@pytest.fixture(
+    params=[
+        (fun, arg) for fun in functions for arg in inspect.signature(fun).parameters.keys()
+    ]
+)
+def funarg(request):
+    return request.param
+
+
+###############################################################################
+# Tests
+
+
+def test_trivial(funarg):
+    function = funarg[0]
+    argument = funarg[1]
+    dsp_function = argdispatch(argument)(function)
+    reg_function = dsp_function.register(str)(function)
+    kwargs = {argument: DISPATCHED}
+    res = reg_function(**kwargs)
+    for arg, value in res.items():
+        if arg == argument:
+            assert value == DISPATCHED
+        else:
+            assert value != DISPATCHED
+
+
+def test_boundmethod():
+    pass
+
+
+def test_abc():
+    pass
 
 
 def test_argdispatch():
